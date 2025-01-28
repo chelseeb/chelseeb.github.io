3021039591a7cc382ca7998b6fa0ead85baa39db
chelseeb <167729587+chelseeb@users.noreply.github.com>
2025-01-28 11:56:22 +0800

    script.js

--- TSA1/script.js
+++ TSA1/script.js
@@ -1,36 +1,36 @@
-let isOriginal = true;
-
+let isResume = true;
 
 document.getElementById('changeButton').addEventListener('click', function () {
     const body = document.body;
     const leftSide = document.querySelector('.left_side');
     const rightSide = document.querySelector('.right_side');
-    const imgBox = document.querySelector('.imgbox');
+    const imgBox = document.querySelector('.imgbox img');
+    const profileText = document.querySelectorAll('.profileText, .title, .contactInfo li');
     const skillsContainer = document.querySelector('.skills');
     const progressBars = document.querySelectorAll('.progress-bar .progress');
 
-    if (isOriginal) {
-       
-        body.style.backgroundImage = "url('bgresume2.png')";
-        leftSide.style.backgroundColor = '#3c0002';
-        rightSide.style.color = '#444';
-        imgBox.style.borderColor = '#f5f5dc';
-        skillsContainer.style.backgroundColor = '#4c2c2e';
-        progressBars.forEach(bar => {
-            bar.style.backgroundColor = '#eecabc'; 
-        });
-    } else {
+    imgBox.style.opacity = '0';
 
-        body.style.backgroundImage = "url('bgresume1.png')";
-        leftSide.style.backgroundColor = '#786200';
-        rightSide.style.color = '#333';
-        imgBox.style.borderColor = '#ffc107';
-        skillsContainer.style.backgroundColor = '#3d3d2f'; 
-        progressBars.forEach(bar => {
-            bar.style.backgroundColor = '#ffe0a3'; 
-        });
-    }
+    setTimeout(() => {
+        if (isResume) {
+            imgBox.src = 'formalpic2.png';
+        } else {
+            imgBox.src = 'formal pic.png';
+        }
 
-  
-    isOriginal = !isOriginal;
+        imgBox.style.opacity = '1';
+
+        body.style.backgroundImage = isResume ? "url('bgresume2.png')" : "url('bgresume1.png')";
+        leftSide.style.backgroundColor = isResume ? '#3c0002' : '#786200';
+        rightSide.style.color = isResume ? '#444' : '#333';
+        profileText.forEach(element => {
+            element.style.color = isResume ? '#f5f5dc' : '#ffe6b6'; 
+        });
+        skillsContainer.style.backgroundColor = isResume ? '#4c2c2e' : '#3d3d2f';
+        progressBars.forEach(bar => {
+            bar.style.backgroundColor = isResume ? '#eecabc' : '#ffe0a3'; 
+        });
+
+        isResume = !isResume;
+    }, 500);
 });
